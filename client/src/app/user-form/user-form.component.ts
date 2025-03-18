5ce4aa3a8207017d45d919bb466aaaebd0572909
maxjtodd <maxtodd@mail.com>
2025-03-18 15:32:02 -0600

    Turned form smart with from tracking

--- client/src/app/user-form/user-form.component.ts
+++ client/src/app/user-form/user-form.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form.component.html',
   styleUrl: './user-form.component.css'
 })
 export class UserFormComponent {
 
+    userForm = new FormGroup({
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        email: new FormControl(''),
+        password: new FormControl('')
+    });
+
+    submit() {
+        console.log(this.userForm.value) 
+    }
 }
