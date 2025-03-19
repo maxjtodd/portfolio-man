1d7852c06fc94eb216904cc4a13dc6843dc349c9
maxjtodd <maxtodd@mail.com>
2025-03-19 13:14:18 -0600

    Navbar now uses user state with conditional rendering

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,5 @@ import { RouterModule } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+    authenticationService: AuthenticationService = inject(AuthenticationService);
 }
