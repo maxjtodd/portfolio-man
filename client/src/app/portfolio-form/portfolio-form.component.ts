11c0da9906bad39564e854b9293c5aa0ecbfdc59
maxjtodd <maxtodd@mail.com>
2025-03-20 13:40:22 -0600

    Created basic form for creating a portfolio

--- client/src/app/portfolio-form/portfolio-form.component.ts
+++ client/src/app/portfolio-form/portfolio-form.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-portfolio-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './portfolio-form.component.html',
   styleUrl: './portfolio-form.component.css'
 })
 export class PortfolioFormComponent {
+
+    portfolioForm = new FormGroup({
+        name: new FormControl(''),
+        private: new FormControl(true)
+    });
+
+    constructor(private router:Router, private auth: AuthenticationService) {}
+
+    ngOnInit() {
+        if (!this.auth.isLoggedIn()) {
+            this.router.navigate(['/'])
+        }
+    }
+
+    submit() {
+        console.log('submitted');
+    }
 
 }
