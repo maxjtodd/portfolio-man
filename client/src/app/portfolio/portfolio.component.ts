87d571d0be4e08640188fcc0a5cf8f29d5c6161c
maxjtodd <maxtodd@mail.com>
2025-03-21 10:29:59 -0600

    Backend fetching if portfolio not passed - handles not found

--- client/src/app/portfolio/portfolio.component.ts
+++ client/src/app/portfolio/portfolio.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Portfolio } from '../portfolio';
+import { Component } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Portfolio } from "../portfolio";
+import { PortfolioService } from "../portfolio.service";
 
 @Component({
-  selector: 'app-portfolio',
-  imports: [],
-  templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.css'
+    selector: "app-portfolio",
+    imports: [],
+    templateUrl: "./portfolio.component.html",
+    styleUrl: "./portfolio.component.css",
 })
 export class PortfolioComponent {
-
     portfolioId = 0;
     portfolio: Portfolio | null = null;
     loadingPortfolio = true;
 
-    constructor(private route: ActivatedRoute, private router: Router) {
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private portfolioService: PortfolioService
+    ) {
         this.portfolioId = Number(this.route.snapshot.params["id"]);
-        this.portfolio = this.router.getCurrentNavigation()?.extras.state as Portfolio | null;
+        this.portfolio = this.router.getCurrentNavigation()?.extras
+            .state as Portfolio | null;
         if (!this.portfolio) {
             this.setPortfolio();
         } else {
@@ -25,8 +30,10 @@ export class PortfolioComponent {
     }
 
     async setPortfolio() {
-        console.log("TODO SET PORTFOLIO VIA THE PORTFOLIO ID AND BACKEND ENDPOINT")
+        const fetchedPortfolio = await this.portfolioService.getPortfolioById(this.portfolioId);
+        if (fetchedPortfolio) {
+            this.portfolio = fetchedPortfolio;
+        }
         this.loadingPortfolio = false;
     }
-
 }
