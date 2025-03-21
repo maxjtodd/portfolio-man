2fc51338ef01bf94c68f190c8835ce74401ab2a9
maxjtodd <maxtodd@mail.com>
2025-03-21 10:59:10 -0600

    Created repository interface and basic implementation

---
+++ server/src/main/java/learn/portfolio_man/data/StockJdbcClientRepository.java
@@ -0,0 +1,35 @@
+package learn.portfolio_man.data;
+
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+import learn.portfolio_man.models.Stock;
+
+@Repository
+public class StockJdbcClientRepository implements StockRepository {
+
+    private final String SELECT = "SELECT stock_id, ticker_symbol FROM stock ";
+
+    private JdbcClient jdbcClient;
+
+    public StockJdbcClientRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+
+    @Override
+    public Stock getById(int stockId) {
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+    @Override
+    public Stock getByTicker(String ticker) {
+        throw new UnsupportedOperationException("Unimplemented method 'getByTicker'");
+    }
+
+    @Override
+    public Stock add(Stock toAdd) {
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    
+}
