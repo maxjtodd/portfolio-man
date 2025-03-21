cff0fa59d3daa7555e7f16e1ac984dcff94f7dfc
maxjtodd <maxtodd@mail.com>
2025-03-21 09:37:37 -0600

    Keeping track of the portfolio object on each page

--- client/src/app/portfolio/portfolio.component.ts
+++ client/src/app/portfolio/portfolio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Portfolio } from '../portfolio';
 
 @Component({
   selector: 'app-portfolio',
@@ -7,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.css'
 })
 export class PortfolioComponent {
+
+    portfolioId = 0;
+    portfolio: Portfolio | null = null;
+    loadingPortfolio = true;
+
+    constructor(private route: ActivatedRoute, private router: Router) {
+        this.portfolioId = Number(this.route.snapshot.params["id"]);
+        this.portfolio = this.router.getCurrentNavigation()?.extras.state as Portfolio | null;
+        if (!this.portfolio) {
+            this.setPortfolio();
+        } else {
+            this.loadingPortfolio = false;
+        }
+    }
+
+    async setPortfolio() {
+        console.log("TODO SET PORTFOLIO VIA THE PORTFOLIO ID AND BACKEND ENDPOINT")
+        this.loadingPortfolio = false;
+    }
 
 }
