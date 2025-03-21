ab8db8ba9845cd3efc77a3c5bf2e3266f839da61
maxjtodd <maxtodd@mail.com>
2025-03-21 11:39:32 -0600

    Implemented and tested getById

--- server/src/main/java/learn/portfolio_man/domain/StockService.java
+++ server/src/main/java/learn/portfolio_man/domain/StockService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import learn.portfolio_man.data.StockRepository;
 import learn.portfolio_man.models.Result;
+import learn.portfolio_man.models.ResultStatus;
 import learn.portfolio_man.models.Stock;
 
 @Service
@@ -16,7 +17,15 @@ public class StockService {
     }
 
     public Result<Stock> getById(int stockId) {
-        return null;
+        Result<Stock> result = new Result<>();
+        Stock fetchedStock = stockRepository.getById(stockId);
+        if (fetchedStock == null) {
+            result.addMessage(ResultStatus.NOT_FOUND, "Stock not found");
+        } else {
+            result.setPayload(fetchedStock);
+        }
+
+        return result;
     }
 
     public Result<Stock> getByTickerSymbol(String tickerSymbol) {
---
+++ server/src/test/java/learn/portfolio_man/domain/StockServiceTest.java
@@ -0,0 +1,55 @@
+package learn.portfolio_man.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import learn.portfolio_man.data.StockRepository;
+import learn.portfolio_man.data.TestHelper;
+import learn.portfolio_man.models.Result;
+import learn.portfolio_man.models.ResultStatus;
+import learn.portfolio_man.models.Stock;
+
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+public class StockServiceTest {
+
+    @Autowired
+    StockService service;
+
+    @MockBean
+    StockRepository stockRepository;
+
+    @Nested
+    class TestGetById {
+
+        @Test
+        void shouldGetByID() {
+            Stock expectedStock = TestHelper.generateStock(1);
+            Result<Stock> expected = new Result<>();
+            expected.setPayload(expectedStock);
+            when(stockRepository.getById(1)).thenReturn(expectedStock);
+
+            Result<Stock> actual = service.getById(1);
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldNotGetByID() {
+            Result<Stock> expected = new Result<>(ResultStatus.NOT_FOUND, "Stock not found");
+            when(stockRepository.getById(1)).thenReturn(null);
+
+            Result<Stock> actual = service.getById(1);
+
+            assertEquals(expected, actual);
+        }
+
+    }
+}
+
