94eaccbccdba45d6ac9e4cbd853b603c640970be
maxjtodd <maxtodd@mail.com>
2025-03-22 17:51:49 -0600

    Implementing buy, added the majority of validation checks and tested

--- server/src/main/java/learn/portfolio_man/domain/HoldingService.java
+++ server/src/main/java/learn/portfolio_man/domain/HoldingService.java
@@ -1,17 +1,70 @@
 package learn.portfolio_man.domain;
 
+import java.math.BigDecimal;
+
 import org.springframework.stereotype.Service;
 
 import learn.portfolio_man.data.HoldingRepository;
+import learn.portfolio_man.data.PortfolioRepository;
+import learn.portfolio_man.data.StockRepository;
+import learn.portfolio_man.models.Holding;
+import learn.portfolio_man.models.Result;
+import learn.portfolio_man.models.ResultStatus;
 
 @Service
 public class HoldingService {
 
     private HoldingRepository holdingRepository;
+    private PortfolioRepository portfolioRepository;
+    private StockRepository stockRepository;
 
-    public HoldingService(HoldingRepository holdingRepository) {
+    public HoldingService(HoldingRepository holdingRepository, PortfolioRepository portfolioRepository,
+            StockRepository stockRepository) {
         this.holdingRepository = holdingRepository;
+        this.portfolioRepository = portfolioRepository;
+        this.stockRepository = stockRepository;
     }
 
+
+    public Result<Holding> buy(Holding toBuy) {
+        Result<Holding> result = validate(toBuy);
+
+        return result;
+    }
     
+
+    private Result<Holding> validate(Holding toValidate) {
+        Result<Holding> result = new Result<>();
+        
+        if (toValidate == null) {
+            result.addMessage(ResultStatus.BAD_REQUEST, "Holding required");
+            return result;
+        }
+
+        if (portfolioRepository.getPortfolioById(toValidate.getPortfolioId()) == null) {
+            result.addMessage(ResultStatus.NOT_FOUND, "Portfolio not found");
+        }
+
+        validateStock(result, toValidate);
+
+        if (toValidate.getAmount() == null || toValidate.getAmount().compareTo(new BigDecimal(0)) <= 0) {
+            result.addMessage(ResultStatus.BAD_REQUEST, "Amount required");
+        }
+
+        return result;
+    }
+
+    private void validateStock(Result<Holding> result, Holding toValidate) {
+
+        if (toValidate.getStock() == null) {
+            result.addMessage(ResultStatus.BAD_REQUEST, "Stock required");
+            return;
+        }
+
+        if (stockRepository.getById(toValidate.getStock().getStockId()) == null) {
+            result.addMessage(ResultStatus.NOT_FOUND, "Stock not found");
+        }
+    }
+
+
 }
---
+++ server/src/test/java/learn/portfolio_man/domain/HoldingServiceTest.java
@@ -0,0 +1,136 @@
+package learn.portfolio_man.domain;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import learn.portfolio_man.data.HoldingRepository;
+import learn.portfolio_man.data.PortfolioRepository;
+import learn.portfolio_man.data.StockRepository;
+import learn.portfolio_man.data.TestHelper;
+import learn.portfolio_man.models.Holding;
+import learn.portfolio_man.models.Result;
+import learn.portfolio_man.models.ResultStatus;
+
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+public class HoldingServiceTest {
+
+    @Autowired
+    HoldingService service;
+
+    @MockBean
+    HoldingRepository holdingRepository;
+
+    @MockBean
+    PortfolioRepository portfolioRepository;
+
+    @MockBean
+    StockRepository stockRepository;
+
+    @Nested
+    class Buy {
+
+        @Test
+        void shouldNotBuyNull() {
+            Holding toBuy = null;
+            Result<Holding> expected = new Result<>(ResultStatus.BAD_REQUEST, "Holding required");
+
+            Result<Holding> actual = service.buy(toBuy); 
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldNotBuyPortolioNotFound() {
+            Holding toBuy = TestHelper.generateHolding(1);
+            setGoodRepositoryStubs(toBuy);
+            when(portfolioRepository.getPortfolioById(toBuy.getPortfolioId())).thenReturn(null);
+            Result<Holding> expected = new Result<>(ResultStatus.NOT_FOUND, "Portfolio not found");
+
+            Result<Holding> actual = service.buy(toBuy); 
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldNotBuyNullStock() {
+            Holding toBuy = TestHelper.generateHolding(1);
+            setGoodRepositoryStubs(toBuy);
+            toBuy.setStock(null);
+            Result<Holding> expected = new Result<>(ResultStatus.BAD_REQUEST, "Stock required");
+
+            Result<Holding> actual = service.buy(toBuy); 
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldNotBuyNonExistantStock() {
+            Holding toBuy = TestHelper.generateHolding(1);
+            setGoodRepositoryStubs(toBuy);
+            when(stockRepository.getById(toBuy.getStock().getStockId())).thenReturn(null);
+            Result<Holding> expected = new Result<>(ResultStatus.NOT_FOUND, "Stock not found");
+
+            Result<Holding> actual = service.buy(toBuy); 
+
+            assertEquals(expected, actual);
+        }
+
+        @Test
+        void shouldNotBuyBadAmount() {
+            Holding toBuy1 = TestHelper.generateHolding(1);
+            Holding toBuy2 = TestHelper.generateHolding(1);
+            Holding toBuy3 = TestHelper.generateHolding(1);
+            setGoodRepositoryStubs(toBuy1);
+            toBuy1.setAmount(null);
+            toBuy2.setAmount(new BigDecimal(0));
+            toBuy3.setAmount(new BigDecimal(-1));
+
+            Result<Holding> expected = new Result<>(ResultStatus.BAD_REQUEST, "Amount required");
+
+            Result<Holding> actual1 = service.buy(toBuy1); 
+            Result<Holding> actual2 = service.buy(toBuy1); 
+            Result<Holding> actual3 = service.buy(toBuy1); 
+
+            assertEquals(expected, actual1);
+            assertEquals(expected, actual2);
+            assertEquals(expected, actual3);
+        }
+
+
+
+        void shouldBuy() {
+            Holding toBuy = TestHelper.generateHolding(1);
+            setGoodRepositoryStubs(toBuy);
+            Result<Holding> expected = new Result<>();
+
+            Result<Holding> actual = service.buy(toBuy); 
+
+            assertEquals(expected, actual);
+        }
+
+    }
+
+
+
+
+
+    private void setGoodRepositoryStubs(Holding holding) {
+        // find good portfolio
+        when(portfolioRepository.getPortfolioById(holding.getPortfolioId()))
+            .thenReturn(TestHelper.generatePortfolio(holding.getPortfolioId()));
+        // find good stock
+        when(stockRepository.getById(holding.getStock().getStockId()))
+            .thenReturn(TestHelper.generateStock(holding.getStock().getStockId()));
+    }
+
+}
+
