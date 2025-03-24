09e54f2d37ccd7393da721a092d153ec105179be
maxjtodd <maxtodd@mail.com>
2025-03-24 17:40:26 -0600

    Add price history route

--- server/src/main/java/learn/portfolio_man/controllers/StockController.java
+++ server/src/main/java/learn/portfolio_man/controllers/StockController.java
@@ -17,6 +17,7 @@ import learn.portfolio_man.domain.StockService;
 import learn.portfolio_man.domain.YahooFinance;
 import learn.portfolio_man.models.Result;
 import learn.portfolio_man.models.Stock;
+import learn.portfolio_man.models.YahooFinance.PriceHistory;
 import learn.portfolio_man.models.YahooFinance.Search;
 import learn.portfolio_man.models.YahooFinance.SearchResult;
 
@@ -85,6 +86,23 @@ public class StockController {
         }
 
         SearchResult searchResult = yahooFinance.searchSpecificStock(ticker);
+        if (searchResult == null) {
+            return ControllerHelper.errorMessageResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong searching");
+        }
+
+        return new ResponseEntity<>(searchResult, HttpStatus.OK);
+    }
+
+    @GetMapping("/priceHistory/{ticker}")
+    public ResponseEntity<Object> getPriceHistory(@PathVariable String ticker, @RequestHeader Map<String, String> headers) {
+        
+        Integer userId = secretSigningKey.getUserIdFromAuthHeaders(headers);
+
+        if (userId == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        PriceHistory searchResult = yahooFinance.getPriceHistory(ticker);
         if (searchResult == null) {
             return ControllerHelper.errorMessageResponse(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong searching");
         }
