6353b6d2c9f568e99dc361a92077b94e77526c45
maxjtodd <maxtodd@mail.com>
2025-03-25 14:19:41 -0600

    Portfolio graph is now working with multiple holdings

--- client/src/app/portfolio/portfolio.component.ts
+++ client/src/app/portfolio/portfolio.component.ts
@@ -79,6 +79,7 @@ export class PortfolioComponent {
             }
             this.priceHistories.push(ph);
         }
+        console.log(this.priceHistories);
         this.loadingGraph = false;
     }
 
