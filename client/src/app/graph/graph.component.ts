419cf539175f9d742c2584ee080886a3efe8b522
maxjtodd <maxtodd@mail.com>
2025-03-25 11:04:44 -0600

    small updates

--- client/src/app/graph/graph.component.ts
+++ client/src/app/graph/graph.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, ViewChild } from '@angular/core';
 import { PriceHistory } from '../price-history';
 import { ChartConfiguration, ChartOptions, ChartType } from "chart.js";
 import { BaseChartDirective } from 'ng2-charts';
+import { PriceHistoryMoment } from '../price-history-moment';
 
 @Component({
     selector: 'app-graph',
@@ -10,7 +11,7 @@ import { BaseChartDirective } from 'ng2-charts';
     styleUrl: './graph.component.css'
 })
 export class GraphComponent {
-    @Input() toGraph!: PriceHistory;
+    @Input() toGraph!: PriceHistory[];
     @Input() name: string = "PriceChart";
     labels?: string[]
     data?: number[]
@@ -21,34 +22,31 @@ export class GraphComponent {
     @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
 
     ngOnInit() {
-        this.labels = Object.keys(this.toGraph.body);
-        this.data = this.labels.map((l) => this.toGraph.body[l].close);
+        this.labels = Object.keys(this.toGraph[0].body);
+        this.data = this.labels.map((l) => this.toGraph[0].body[l].close);
 
         this.lineChartData = {
             datasets: [
                 {
+                    label: "Data",
                     data: this.data,
-                    borderColor: 'rgba(0, 0, 0, 0)',
-                    pointBorderColor: 'black',
-                    pointHoverBackgroundColor: 'red',
-                    pointHoverBorderColor: 'rgba(148,159,177,0.8)',
                 },
             ],
             labels: this.labels
         };
 
         this.lineChartOptions = {
-            elements: {
-                line: {
-                    tension: 0.5,
-                },
-            },
-            scales: {
-                y: {
-                    position: 'left',
-                },
-            },
-
+            // elements: {
+            //     line: {
+            //         tension: 0.5,
+            //     },
+            // },
+            // scales: {
+            //     y: {
+            //         position: 'left',
+            //     },
+            // },
+            //
         };
 
     }
