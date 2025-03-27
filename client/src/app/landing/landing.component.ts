11f12456d6113e568bc7833518a1f223d2c3c0a8
maxjtodd <maxtodd@mail.com>
2025-03-26 20:16:09 -0600

    Landing page overhaul

--- client/src/app/landing/landing.component.ts
+++ client/src/app/landing/landing.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
